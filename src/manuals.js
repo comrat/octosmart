12341de3b316227b2ef374f0eed2628911650cd2
Alexey Pobery <poberyaa@gmail.com>
2019-01-07 23:20:55 +0300

    Remove manuals they are useless now

--- src/manuals.js
+++
@@ -1,9 +0,0 @@
-/*** @using { manuals.CivManual } **/
-/*** @using { manuals.PlanetManual } **/
-/*** @using { manuals.Manual2048 } **/
-/*** @using { manuals.SkywardManual } **/
-/*** @using { manuals.T8nksManual } **/
-/*** @using { manuals.OctopegManual } **/
-/*** @using { manuals.KeshaManual } **/
-/*** @using { manuals.KeshaWasNinjaManual } **/
-/*** @using { manuals.DvnManual } **/
